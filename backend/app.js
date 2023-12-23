83cb0736a5a88897e1c0fbe8aaf8158191f2dec0
ayesha0504 <116142078+ayesha0504@users.noreply.github.com>
2023-12-22 20:14:59 -0600

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index.js');
+var usersRouter = require('./routes/users');
+var reservationsRouter = require('./routes/reservations');
+var propertyRouter = require('./routes/properties');
+var favouriteRouter = require('./routes/favourites');
+var ratingsRouter = require('./routes/ratings');
+
+var app = express();
+
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST');
+  res.append('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-auth-token');
+  next();
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/reservations', reservationsRouter);
+app.use('/properties', propertyRouter);
+app.use('/favourites', favouriteRouter);
+app.use('/ratings', ratingsRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
